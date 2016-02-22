e1aed781bec4cfb5e43502cc6089a2cfe102fd87
Rakesh TA <rakeshta@gmail.com>
2016-02-22 10:12:37 -0500

    added documentation to the peripheral class + fixed missing connection established delegate callback + made all delegate methods option + added name updated delegate callback

--- BLESerial/BLESerial/BLESerial.h
+++ BLESerial/BLESerial/BLESerial.h
@@ -8,12 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-////! Project version number for BLESerial.
-//FOUNDATION_EXPORT double BLESerialVersionNumber;
-//
-////! Project version string for BLESerial.
-//FOUNDATION_EXPORT const unsigned char BLESerialVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <BLESerial/PublicHeader.h>
 
 
--- BLESerial/BLESerial/BLESerialPeripheral.swift
+++ BLESerial/BLESerial/BLESerialPeripheral.swift
@@ -13,19 +13,52 @@ import CocoaLumberjackSwift
 
 // MARK: - BLESerialPeripheralDelegate
 
+/// The `BLESerialPeripheralDelegate` protocol defines methods that must be adopted
+/// to be notified of connection, data events etc.
+@objc
 public protocol BLESerialPeripheralDelegate: AnyObject {
-    func serialPeripheralDidConnect(serialPeripheral: BLESerialPeripheral)
-    func serialPeripheral(serialPerihperal: BLESerialPeripheral, didDisconnectWithError error: NSError?)
-    func serialPeripheral(serialPerihperal: BLESerialPeripheral, didFailToConnectWithError error: NSError?)
+    
+    // MARK: - Connection Events
+    
+    /// Invoked when a connection is succesfully created with the peripheral.
+    ///
+    /// - parameter serialPeripheral: The peripheral that has been connected.
+    optional func serialPeripheralDidConnect(serialPeripheral: BLESerialPeripheral)
+    
+    /// Invoked when an existing connection to a peripheral is torn down.
+    ///
+    /// - parameter serialPerihperal: The peripheral that was disconnected.
+    /// - parameter error:            The cause of the failure if any.
+    optional func serialPeripheral(serialPerihperal: BLESerialPeripheral, didDisconnectWithError error: NSError?)
+    
+    /// Invoked when a connection could not be established with the peripheral.
+    ///
+    /// - parameter serialPerihperal: The peripheral that failed to connect.
+    /// - parameter error:            The cause of the failure.
+    optional func serialPeripheral(serialPerihperal: BLESerialPeripheral, didFailToConnectWithError error: NSError?)
+    
+    
+    // MARK: - Meta Events
+    
+    /// Invoked when the peripheral's name changes
+    ///
+    /// - parameter serialPerihperal: The peripheral that changed its name.
+    /// - parameter name:             The new name of the peripheral.
+    optional func serialPeripheral(serialPerihperal: BLESerialPeripheral, didUpdateName name: String?)
 }
 
 
 // MARK: - BLESerialPeripheral
 
+/// `BLESerialPeripheral` is a thin wrapper around a CoreBluetooth peripheral
+/// that supports serial communication.
+/// 
+/// - seealso: `BLESerialManager`
 public final class BLESerialPeripheral: NSObject {
     
     // MARK: - Members
     
+    /// That delegate object that should receive peripheral events.
     public weak var delegate:            BLESerialPeripheralDelegate?
 
     
@@ -47,14 +80,19 @@ public final class BLESerialPeripheral: NSObject {
     
     // MARK: - Accessors
     
+    /// The unique identifier associated with the peripheral.
+    ///
+    /// - seealso: `CBPeripheral.identifier` for more information.
     public   var identifier:             NSUUID {
         return cbPeripheral.identifier
     }
     
+    /// The human-readable name of the peripheral.
     public   var name:                   String? {
         return cbPeripheral.name
     }
     
+    /// The current connection state of the peripheral.
     public   var state:                  CBPeripheralState {
         return cbPeripheral.state
     }
@@ -62,6 +100,7 @@ public final class BLESerialPeripheral: NSObject {
     
     // MARK: -
     
+    /// The local name of the device if any.
     public  var localName:          String? {
         return advertisementData[CBAdvertisementDataLocalNameKey] as? String
     }
@@ -87,11 +126,29 @@ public final class BLESerialPeripheral: NSObject {
 
 extension BLESerialPeripheral {
     
+    /// Establishes a local connection to the peripheral.
+    ///
+    /// As part of connecting, the wrapper also interrogates the device for it's
+    /// services and characteristics.
+    ///
+    /// If a connection is succesfully established, the `serialPeripheralDidConnect:`
+    /// method of the delegate is invoked. If the connection attempt fails, the
+    /// `serialPeripheral:didFailToConnectWithError:` method of the delegate is
+    /// invoked.
+    ///
+    /// - parameter completion: A handler to be invoked when the connection attempt
+    ///   succeeds or fails.
+    ///
+    /// - seealso: `disconnect`
     public func connect(completion completion: ((success: Bool, error: NSError?) -> Void)?) {
         onConnectCallback = completion
         serialManager.connectPeripheral(self)
     }
     
+    /// Disconnects or cancels an active or pending connection to the peripheral.
+    ///
+    /// On disconnect, the `serialPeripheral:didDisconnectWithError:` method of
+    /// the delegate is invoked.
     public func disconnect() {
         
         // Cleanup callbacks
@@ -122,7 +179,7 @@ extension BLESerialPeripheral {
         onConnectCallback = nil
         
         // Notify delegate
-        delegate?.serialPeripheral(self, didDisconnectWithError: error)
+        delegate?.serialPeripheral?(self, didDisconnectWithError: error)
     }
     
     func onDidFailToConnect(error error: NSError?) {
@@ -132,14 +189,20 @@ extension BLESerialPeripheral {
         onConnectCallback = nil
         
         // Notify delegate
-        delegate?.serialPeripheral(self, didFailToConnectWithError: error)
+        delegate?.serialPeripheral?(self, didFailToConnectWithError: error)
     }
     
     
     // MARK: -
     
-    func onDidDiscoverServices() {
+    func onDidDiscoverServicesAndCharacteristics() {
         
+        // Invoke connection callback if any
+        onConnectCallback?(success: true, error: nil)
+        onConnectCallback = nil
+        
+        // Notify delegate
+        delegate?.serialPeripheralDidConnect?(self)
     }
 }
 
@@ -165,10 +228,9 @@ extension BLESerialPeripheral: CBPeripheralDelegate {
             }
         }
         
-        // Invoke connection callback if any if there are no services
+        // Finish discovery if there are no services
         else {
-            onConnectCallback?(success: true, error: nil)
-            onConnectCallback = nil
+            onDidDiscoverServicesAndCharacteristics()
         }
     }
     
@@ -193,10 +255,22 @@ extension BLESerialPeripheral: CBPeripheralDelegate {
         }
         
         
-        // Invoke connection callback when all characteristics for all services have been discovered
+        // Finish discovery when all characteristics for all services have been discovered
         if  cbPeripheral.services!.filter({ $0.characteristics == nil }).count == 0 {
-            onConnectCallback?(success: true, error: nil)
-            onConnectCallback = nil
+            onDidDiscoverServicesAndCharacteristics()
         }
+    }
+    
+    
+    // MARK: -
+    
+    public func peripheralDidUpdateName(peripheral: CBPeripheral) {
+        
+        // Log event
+        let name = peripheral.name
+        DDLogVerbose("\(CurrentFileName()): Peripheral \(peripheral) update its name - \(name ?? "<nil>")")
+        
+        // Notify delegate
+        delegate?.serialPeripheral?(self, didUpdateName: name)
     }
 }
